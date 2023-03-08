8cb37e3119ef2460106bbfe09c2ab12dda569893
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-03-08 22:59:07 +0200

    add  highlighted cells

--- src/Matrix.tsx
+++ src/Matrix.tsx
@@ -4,6 +4,7 @@ import { useMatrixContext } from "./MatrixContext";
 const Matrix: React.FC = () => {
   const { rows, cols } = useMatrixContext();
   const [matrix, setMatrix] = useState<number[][]>([]);
+  const [highlightedCells, setHighlightedCells] = useState<number[][]>([]);
 
   const generateMatrix = () => {
     const newMatrix = [];
@@ -37,23 +38,68 @@ const Matrix: React.FC = () => {
     setMatrix(newMatrix);
   };
 
+
+  const findClosestCells = (matrix: number[][], rowIndex: number, cellIndex: number): number[][] => {
+    const currentCellValue = matrix[rowIndex][cellIndex];
+    let closestValueDiff = Infinity;
+    let closestCells: number[][] = [];
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (i === rowIndex && j === cellIndex) continue;
+  
+        const cellValue = matrix[i][j];
+        const valueDiff = Math.abs(cellValue - currentCellValue);
+  
+        if (valueDiff < closestValueDiff) {
+          closestValueDiff = valueDiff;
+          closestCells = [[i, j]];
+        } else if (valueDiff === closestValueDiff) {
+          closestCells.push([i, j]);
+        }
+      }
+    }
+  
+    return closestCells;
+  };
+  
+  const handleCellMouseEnter = (rowIndex: number, cellIndex: number) => {
+    const closestCells = findClosestCells(matrix, rowIndex, cellIndex);
+    setHighlightedCells(closestCells);
+  };
+
+  const handleCellMouseLeave = () => {
+    setHighlightedCells([]);
+  };
+
   return (
     <div className="matrix">
       <button onClick={generateMatrix}>
         <span> Generate Matrix</span>
       </button>
       <table>
-        <tbody>
+      <tbody>
           {matrix.map((row, rowIndex) => (
             <tr key={`row-${rowIndex}`}>
-              {row.map((cell, cellIndex) => (
-                <td
-                  key={`cell-${rowIndex}-${cellIndex}`}
-                  onClick={() => handleCellClick(rowIndex, cellIndex)}
-                >
-                  {cell}
-                </td>
-              ))}
+              {row.map((cell, cellIndex) => {
+                const isHighlighted = highlightedCells.some(
+                  ([highlightedRowIndex, highlightedCellIndex]) =>
+                    highlightedRowIndex === rowIndex &&
+                    highlightedCellIndex === cellIndex
+                );
+
+                return (
+                  <td
+                    key={`cell-${rowIndex}-${cellIndex}`}
+                    onClick={() => handleCellClick(rowIndex, cellIndex)}
+                    onMouseEnter={() => handleCellMouseEnter(rowIndex, cellIndex)}
+                    onMouseLeave={handleCellMouseLeave}
+                    style={{ background: isHighlighted ? "rgb(200 112 9)" : "" }}
+                  >
+                    {cell}
+                  </td>
+                );
+              })}
               <td>{row.reduce((a, b) => a + b)}</td>
             </tr>
           ))}
